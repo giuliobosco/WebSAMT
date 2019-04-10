e1593bb50685f973035a5df206850c330c22c4f2
giuliobosco <giuliobva@gmail.com>
2019-04-10 10:48:11 +0200

    fix test_2 views utenti index - row of table

--- test_2/application/views/utenti/index.php
+++ test_2/application/views/utenti/index.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>utenti</title>
+	<meta charset="utf-8">
+	<title>utenti</title>
 </head>
 <body>
 
 <table>
-    <tr>
-        <th>email</th>
-        <th>nome</th>
-        <th>cognome</th>
-        <th>nascita</th>
-        <th>password</th>
-        <th></th>
-    </tr>
-    <?php foreach ($utenti as $utente): ?>
-        <td><?php echo($utente->getEmail()); ?></td>
-        <td><?php echo($utente->getNome()); ?></td>
-        <td><?php echo($utente->getCognome()); ?></td>
-        <td><?php echo($utente->getNascita()); ?></td>
-        <td><?php echo($utente->getPassword()); ?></td>
-        <td>
-            <a href="<?php echo URL . "utenti/delete/"; echo($utente->getEmail()); ?>">DELETE</a>
-        </td>
-    <?php endforeach; ?>
+	<tr>
+		<th>email</th>
+		<th>nome</th>
+		<th>cognome</th>
+		<th>nascita</th>
+		<th>password</th>
+		<th></th>
+	</tr>
+
+	<?php foreach ($utenti as $utente): ?>
+		<tr>
+			<td><?php echo($utente->getEmail()); ?></td>
+			<td><?php echo($utente->getNome()); ?></td>
+			<td><?php echo($utente->getCognome()); ?></td>
+			<td><?php echo($utente->getNascita()); ?></td>
+			<td><?php echo($utente->getPassword()); ?></td>
+			<td>
+				<a href="<?php echo URL . "utenti/delete/";
+				echo($utente->getEmail()); ?>">DELETE</a>
+			</td>
+		</tr>
+	<?php endforeach; ?>
 </table>
 
 </body>
