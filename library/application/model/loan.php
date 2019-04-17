6c2a433885c8747ded1acfb9d87b67a6db08aba5
giuliobosco <giuliobva@gmail.com>
2019-04-17 08:54:59 +0200

    add library loan

---
+++ library/application/model/loan.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * The MIT License
+ *
+ * Copyright 2018 giuliobosco.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+/**
+ * @author giuliobosco
+ * @version 1.0 (2019-04-17 - 2019-04-17)
+ */
+class loan extends model {
+
+	private $book;
+
+	private $user;
+
+	private $loan_date;
+
+	private $return_date;
+
+	public function __construct(int $id, $book, $user, $loan_date, $return_date = null) {
+		parent::__construct($id);
+
+		$this->book = $book;
+		$this->user = $user;
+		$this->loan_date = $loan_date;
+		$this->return_date = $return_date;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getBook() {
+		return $this->book;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getUser() {
+		return $this->user;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getLoanDate() {
+		return $this->loan_date;
+	}
+
+	/**
+	 * @return null
+	 */
+	public function getReturnDate() {
+		return $this->return_date;
+	}
+}
